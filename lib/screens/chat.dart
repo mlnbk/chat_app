3ad8bdc80cd909f0ed8d8f3cba47127cf4ff8b16
mlnbk <dev.milan.bako@gmail.com>
2023-09-25 13:02:37 +0200

    feat: add push notificiation functionality
    * receive messages on a message topic

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,29 @@
-import 'package:chat_app/widgets/chat_messages.dart';
-import 'package:chat_app/widgets/new_message.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    await FirebaseMessaging.instance.requestPermission();
+    await FirebaseMessaging.instance.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +32,14 @@ class ChatScreen extends StatelessWidget {
         title: const Text('FlutterChat'),
         actions: [
           IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(Icons.exit_to_app,
-                  color: Theme.of(context).colorScheme.primary))
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
         ],
       ),
       body: const Column(
